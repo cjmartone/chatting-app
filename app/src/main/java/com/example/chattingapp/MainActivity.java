0ae9803362b1f5765518a4a4c907d24976447b32
Christopher Martone <cjm74@njit.edu>
2020-04-11 00:30:59 -0400

    add realtime update and scrollview

--- app/src/main/java/com/example/chattingapp/MainActivity.java
+++ app/src/main/java/com/example/chattingapp/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.chattingapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,31 +24,72 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.example.chattingapp.R.drawable.message_sent;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore db;
 
-    private TextView messageBox;
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layout = findViewById(R.id.messageLayout);
         firebaseAuth = FirebaseAuth.getInstance();
         signInUser();
 
         db = FirebaseFirestore.getInstance();
+        setListener();
+    }
 
-        messageBox = findViewById(R.id.messageBox);
+    public void setListener(){
+        db.collection("friends").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException e) {
+                if(e != null){
+                    System.out.println("There was an issue listening to the db");
+                    return;
+                }
+
+                for(QueryDocumentSnapshot doc : value){
+                    ArrayList<String> messages = (ArrayList<String>)doc.getData().get("messages");
+                    for(String message : messages){
+                        createSentBubble(message);
+                    }
+                }
+            }
+        });
+    }
+
+    public void createSentBubble(String message){
+        TextView view = new TextView(layout.getContext());
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.weight = 1.0f;
+        layoutParams.gravity = Gravity.BOTTOM|Gravity.RIGHT;
+
+        view.setText(message);
+        view.setTextColor(Color.WHITE);
+        view.setTextSize(18);
+        view.setBackgroundResource(message_sent);
+        view.setPadding(20, 10, 20, 10);
+        view.setLayoutParams(layoutParams);
+
+        layout.addView(view);
     }
 
     public void sendMessage(View view){
@@ -61,24 +111,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         editText.setText("");
-        getMessages();
-    }
-
-    public void getMessages(){
-        db.collection("messages")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
-                            String messages = "";
-                            for(QueryDocumentSnapshot document : task.getResult()){
-                                messages += document.getData() + "\n";
-                            }
-                            messageBox.setText(messages);
-                        }
-                    }
-                });
     }
 
     public void signInUser(){
